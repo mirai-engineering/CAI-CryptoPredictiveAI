297ed95bfde2ea6b61e13e4d48a8047e408bcb77
Paulescu <plabartabajo@gmail.com>
2025-04-07 13:02:07 +0200

    Makes the kafka install scripts executable

--- deployments/dev/kind/create_cluster.sh
+++ deployments/dev/kind/create_cluster.sh
@@ -18,8 +18,12 @@ echo "Creating the cluster..."
 KIND_EXPERIMENTAL_DOCKER_NETWORK=rwml-34fa-network kind create cluster --config ./kind-with-portmapping.yaml
 
 # 5. Install Kafka
+echo "Installing Kafka..."
+chmod +x ./install_kafka.sh
 ./install_kafka.sh
 
 # 6. Install Kafka UI
+echo "Installing Kafka UI..."
+chmod +x ./install_kafka_ui.sh
 ./install_kafka_ui.sh
 
