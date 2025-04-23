074fd965c54007b8ac41250bda1a1fe3a337c1d8
brunoclbr <bruno.copa@rwth-aachen.de>
2025-04-23 15:45:51 +0000

    edited postcreatecommand.sh to fix locale warning

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
+echo "Fixing locale..."
+sudo apt-get update && sudo apt-get install -y locales
+sudo locale-gen en_US.UTF-8
+sudo update-locale LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+export LANGUAGE=en_US:en
+export LC_ALL=
 # Install tools specified in mise.toml
 #
 cd /workspaces/real-time-ml-system-cohort-4
