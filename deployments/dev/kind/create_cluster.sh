7fef67e0be755f06527ef135b3044e2895067513
brunoclbr <bruno.copa@rwth-aachen.de>
2025-05-07 13:33:09 -0400

    added risingwave installation to create cluster and tried hard-coded query on psql with k8s

--- deployments/dev/kind/create_cluster.sh
+++ deployments/dev/kind/create_cluster.sh
@@ -27,3 +27,8 @@ echo "Installing Kafka UI..."
 chmod +x ./install_kafka_ui.sh
 ./install_kafka_ui.sh
 
+# 7. Install RisingWave
+echo "Installing RisingWave..."
+chmod 755 ./install_risingwave.sh
+./install_risingwave.sh
+
