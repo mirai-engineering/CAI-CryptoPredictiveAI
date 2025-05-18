74704d07387a8a87100ec0c80ecdd3a6eee4059a
brunoclbr <bruno.copa@rwth-aachen.de>
2025-05-18 09:44:36 +0200

    adds kustomize to deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -25,6 +25,11 @@ echo "KUBECONFIG=${KUBECONFIG}"
 
 # if there is a kustomization.yaml file, use kustomize to deploy the service
 if [ -f ${service}/kustomization.yaml ]; then
+    echo "Deploying ${service} with kustomize"
+    # delete the service
+    # TODO: add the ignore-not-found flag to avoid errors the first time you deploy something
+    kustomize build ${service} | kubectl delete -f -
+    # deploy the service
     kustomize build ${service} | kubectl apply -f -
 else
     # manually apply the deployment manifests
