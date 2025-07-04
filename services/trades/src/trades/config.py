0d6bf7bc0b70fb5fece9c04e03cc57a37449b444
Bruno Copa <bruno.copa.97@gmail.com>
2025-07-04 14:57:02 +0200

    deleted currencies that I dont need. Proceed to create and deploy new image

--- services/trades/src/trades/config.py
+++ services/trades/src/trades/config.py
@@ -9,22 +9,20 @@ class Settings(BaseSettings):
         env_file='services/trades/settings.env', env_file_encoding='utf-8'
     )
 
-    product_ids: list[str] = [
-        'ETH/EUR',
-        'BTC/USD',
-        'BTC/EUR',
-        'ETH/USD',
-        'SOL/USD',
-        'SOL/EUR',
-        'XRP/USD',
-        'XRP/EUR',
-    ]
+    product_ids: list[str] = ['ETH/EUR', 'BTC/USD']
+    #'BTC/EUR',
+    #'ETH/USD',
+    #'SOL/USD',
+    #'SOL/EUR',
+    #'XRP/USD',
+    #'XRP/EUR',
+
     kafka_broker_address: str
     kafka_topic_name: str
     live_or_historical: Literal['live', 'historical'] = Field(
         'live', env='LIVE_OR_HISTORICAL'
     )
-    last_n_days: int = 30
+    last_n_days: int = 10
 
 
 config = Settings()
